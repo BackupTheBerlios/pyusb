7248f02912f6232c7fe0463b5f72d792d95e9f77
wander <wander>
2005-10-09 22:28:26 +0000

    - Some setup improvements

--- pyusb/setup.py
+++ pyusb/setup.py
@@ -22,10 +22,17 @@ if -1 != platform.find("mac"):
 elif -1 != platform.find("win32"):
 	libraries = ["libusb"]	
 
-usbmodule = Extension('usb',
+usbmodule = Extension(name = 'usb',
 					libraries = libraries,
 					sources = ['pyusb.c'],
 					extra_link_args = extra_link_args,
-					extra_compile_args = extra_compile_args)
+					extra_compile_args = extra_compile_args,
+					depends = ['pyusb.h'])
 
-setup(ext_modules = [usbmodule])
+setup(name = 'pyusb',
+	version = '0.2.1',
+	author = 'Wander Lairson Costa',
+	author_email = 'wander.lairson@gmail.com',
+	url = 'http://pyusb.berlios.de',
+	license = 'GPL',
+	ext_modules = [usbmodule])
